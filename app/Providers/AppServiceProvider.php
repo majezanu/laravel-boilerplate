ce0a4b1102399699c3b83199c9cbd3a0ef41a884
Manuel Zavala <majezanu@jarvixpop.lan>
2020-06-18 23:22:46 -0500

    feat: setup telescope only on dev env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if ($this->app->isLocal()) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 }
